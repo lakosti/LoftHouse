a54620fa899f7793165d783282139429dcfe7d3d
Yaroslava <yaroslava.hovenko@gmail.com>
2025-01-09 21:09:18 +0200

    add phone mask for inputs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import mask from "./libs/phoneMask/mask.js";
+
 const btn = document.querySelector(".nav-icon-btn");
 const icon = document.querySelector(".nav-icon");
 const menu = document.querySelector(".header__top-row");
@@ -7,4 +9,17 @@ btn.addEventListener("click", () => {
   menu.classList.toggle("header__top-row--mobile");
 
   document.body.classList.toggle("no-scroll");
+});
+
+//phone mask
+mask("[data-tel]"); //пошук по атрибуту
+
+const inputs = document.querySelectorAll("[data-tel]");
+inputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    if (input.value == "+") input.value = "";
+  });
+  input.addEventListener("blur", () => {
+    if (input.value == "+") input.value = "";
+  });
 });
